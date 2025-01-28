ebf7491f0af7294eb1c7fc404b02c83270d7d4a0
Akhil Pandey <akhilpandey494@gmail.com>
2025-01-28 12:22:58 +0530

    Bubble Sort Linked List.c

---
+++ Bubble Sort Linked List.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+void appendNode(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+void bubbleSort(struct Node* head) {
+    if (head == NULL) return;
+
+    int swapped;
+    struct Node *ptr1;
+    struct Node *lptr = NULL; 
+    do {
+        swapped = 0;
+        ptr1 = head;
+
+        while (ptr1->next != lptr) {
+            if (ptr1->data > ptr1->next->data) {
+                int temp = ptr1->data;
+                ptr1->data = ptr1->next->data;
+                ptr1->next->data = temp;
+
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+}
+int main() {
+    struct Node* head = NULL;
+
+    appendNode(&head, 45);
+    appendNode(&head, 10);
+    appendNode(&head, 30);
+    appendNode(&head, 5);
+    printf("Original list:\n");
+    printList(head);
+    bubbleSort(head);
+    printf("Sorted list:\n");
+    printList(head);
+    return 0;
+}
